9135c56337fc7fc56ed3e53ba564c4b11d2c6bd7
Suren1987 <46677019+Suren1987@users.noreply.github.com>
2019-10-04 12:59:07 -0400

    Final version of script 2

--- Script2.py
+++ Script2.py
@@ -1,25 +1,43 @@
-# Name: Aggeliki Barberopoulou 
-# Assignment title: Lab 3 in class exercise
-# Time to complete: 15 mins.
-# Created: September 9th 2019
-# Modified: October 2nd, 2019
-# Description: This script calculates rain runoff for a single 
-# small area of land and a given rainfall
+# Name: Aggeliki Barberopoulou and Surendra Shrestha
+# Assignment title: Final Project
+# Time to complete: 1 hour.
+# Created: October 4th 2019
+# Modified: October 5th, 2019
+# Description: This script calculates rain runoff for a watershed based on the user input value of precipitation
 
-l=float(input('Enter length of area in feet:'))
-w=float(input('Enter width of area in feet:'))
+#Terms
+# P = precipitation (inches/day)
+# ET = evapotranspiration (inches/day)
+# R = Runoff depth (inches/day)
+# L = Length of the watershed (meters)
+# B = Breadth of the watershed (meters)
+# A = Area of the field (meters squared)
 
-length=l*12 # length of land in inches
-print "Area length is:", l # print length in inches
-width=w*12 # width of land in inches
-print "Area width is:", w # print width in inches
-Area = w*l # area of land in square inches
-print "Area of land is:", Area ," square feet" # print area
+def calculate_runoff(P):
+    ET = 0.6*P  # Evapotranspiration (ET) in inches per day
+    R = P - ET # Runoff (R) in inches per day
+    return R
 
-rainwater=input('Enter amount of rain in inches:') #Enter amount of rainfall at prompt
-rainfall_inches = Area*rainwater # Rainfall in cubic inches
-runoff_gallons=rainfall_inches*0.004329 # Convert rainfall to gallons
+P = input('Enter a value for precipitation: ') # User input value of precipitation in inches per day
+Runoff = calculate_runoff(P) # Assign variable to the output of runoff function
 
-# Print results
-print "Rainfall_inches is:", rainfall_inches
-print "runoff_gallons is:", runoff_gallons
+# Convert inches to meters
+Runoff_meter = Runoff * 0.0254 # Converting runoff from inches to meters per day
+print "The runoff depth in meters per day: ", Runoff_meter
+
+
+### Calculate Area of the field
+def calculate_area(L, B): #Both length and breadth are in meters
+    A = L * B
+    return A
+    
+L = input('Enter a value for length of field: ') # User input value of length in meters
+B = input('Enter a value for breadth of field: ') # User input value of breadth in meters
+Area = calculate_area(L, B) # Assign variable to the output of function
+print "The area of the watershed in meter squared : ", Area
+
+#Calculate Streamflow
+Streamflow = Runoff_meter * Area # Calculating streamflow in cubic meter per day
+print "The streamflow in cubic meters per day: ", Streamflow
+
+
